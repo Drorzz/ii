01754ed452b87337f74297ca0765def4a1689fe1
Drorzz <arhorus@gmail.com>
2014-08-07 10:53:52 +0300

    Add @Autowired

--- ii-seo/src/test/java/org/ayfaar/ii/seo/impl/SEOViewThymeleafSpringConfigurationForTest.java
+++ ii-seo/src/test/java/org/ayfaar/ii/seo/impl/SEOViewThymeleafSpringConfigurationForTest.java
@@ -1,7 +1,6 @@
 package org.ayfaar.ii.seo.impl;
 
 import org.ayfaar.ii.seo.SEOView;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -14,7 +13,6 @@ import org.springframework.context.annotation.PropertySource;
 public class SEOViewThymeleafSpringConfigurationForTest {
 
     @Bean
-    @Autowired
     public SEOView testView(){
         return new SEOViewThymeleafImplMock("testView");
     }
