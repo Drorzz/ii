e4df943c66ccd787113115fb5c0b9e3c1d05dcb2
Drorzz <arhorus@gmail.com>
2014-08-07 12:36:57 +0300

    Refactoring

--- ii-seo/src/test/java/org/ayfaar/ii/seo/impl/AbstractSEOViewThymeleafImplTest.java
+++ ii-seo/src/test/java/org/ayfaar/ii/seo/impl/AbstractSEOViewThymeleafImplTest.java
@@ -4,6 +4,9 @@ import org.ayfaar.ii.seo.SEOView;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -38,5 +41,14 @@ public class AbstractSEOViewThymeleafImplTest {
         assertFalse("Param 1",result.contains("<p>param1</p>"));
         assertFalse("Param 2",result.contains("<p>param2</p>"));
         assertFalse("Param 3",result.contains("<p>param3</p>"));
+    }
+}
+
+@Configuration
+@PropertySource("classpath:seoThymeleafSpring_test.properties")
+class SEOViewThymeleafSpringConfigurationForTest {
+    @Bean
+    public SEOView testView(){
+        return new SEOViewThymeleafImplMock("testView");
     }
 }
